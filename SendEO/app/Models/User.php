01246d52fb21aba524534c146e1e05095c4135ba
Pro-ITDevSuccess <itdevsuccess@gmail.com>
2023-03-13 12:54:00 +0300

    SAVE

--- SendEO/app/Models/User.php
+++ SendEO/app/Models/User.php
@@ -1,8 +1,13 @@
+<?php
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-
+    protected $table = 'users';
+    protected $primaryKey='id';
+    protected $fillable= ['firstname', 'lastname', 'email', 'phone', ];
 }
+
+?>
